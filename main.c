bdfe85f57753c087f1f710cf2a2defbde5f8c775
Holger Langenau <holger.langenau@mathematik.tu-chemnitz.de>
2015-04-04 12:42:28 +0200

    Add message window.

--- main.c
+++ main.c
@@ -2,27 +2,51 @@
 #include <glib.h>
 #include <gtk/gtk.h>
 
+GtkTextBuffer *text_buffer = NULL;
+GtkAdjustment *vadjustment = NULL;
+
 gboolean mt_window_button_event(GtkWidget *widget, GdkEventButton *event, gpointer data)
 {
-    fprintf(stdout, "%" G_GUINT32_FORMAT " button event: ", event->time);
+    GString *msg = g_string_sized_new(256);
+    GtkTextIter tend;
+
+    g_string_printf(msg, "%" G_GUINT32_FORMAT ": ", event->time);
     switch (event->type) {
         case GDK_BUTTON_PRESS:
-            fprintf(stdout, "press ");
+            g_string_append_printf(msg, "press ");
             break;
         case GDK_2BUTTON_PRESS:
-            fprintf(stdout, "2 button press ");
+            g_string_append_printf(msg, "2 button press ");
             break;
         case GDK_3BUTTON_PRESS:
-            fprintf(stdout, "3 button press ");
+            g_string_append_printf(msg, "3 button press ");
             break;
         case GDK_BUTTON_RELEASE:
-            fprintf(stdout, "release ");
+            g_string_append_printf(msg, "release ");
             break;
         default:
-            fprintf(stdout, "unhandled event ");
+            g_string_append_printf(msg, "unhandled event ");
     }
 
-    fprintf(stdout, "%u\n", event->button);
+    if (event->button == 1)
+        g_string_append_printf(msg, "LMB\n");
+    else if (event->button == 2)
+        g_string_append_printf(msg, "MMB\n");
+    else if (event->button == 3)
+        g_string_append_printf(msg, "RMB\n");
+    else
+        g_string_append_printf(msg, "%u\n", event->button);
+
+    gchar *str = g_string_free(msg, FALSE);
+
+    gtk_text_buffer_get_iter_at_offset(text_buffer, &tend, -1);
+    gtk_text_buffer_insert(text_buffer, &tend, str, -1);
+
+    fputs(str, stdout);
+    fflush(stdout);
+    g_free(str);
+
+    gtk_adjustment_set_value(vadjustment, gtk_adjustment_get_upper(vadjustment)); 
 
     return TRUE;
 }
@@ -46,16 +70,34 @@ int main(int argc, char **argv)
     if (win == NULL)
         return 1;
 
+    GtkWidget *event_box = gtk_event_box_new();
+    GtkWidget *frame = gtk_frame_new("Click area");
+    gtk_container_add(GTK_CONTAINER(frame), event_box);
+
+    GtkWidget *text_view = gtk_text_view_new();
+    GtkWidget *scrolled_win = gtk_scrolled_window_new(NULL, NULL);
+    vadjustment = gtk_scrolled_window_get_vadjustment(GTK_SCROLLED_WINDOW(scrolled_win));
+    gtk_container_add(GTK_CONTAINER(scrolled_win), text_view);
+
+    gtk_text_view_set_editable(GTK_TEXT_VIEW(text_view), FALSE);
+    text_buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
+
+    GtkWidget *vbox = gtk_vbox_new(TRUE, 3);
+    gtk_box_pack_start(GTK_BOX(vbox), frame, TRUE, TRUE, 2);
+    gtk_box_pack_start(GTK_BOX(vbox), scrolled_win, TRUE, TRUE, 2);
+    gtk_container_add(GTK_CONTAINER(win), vbox);
+    gtk_widget_show_all(vbox);
+
     gtk_window_set_default_size(GTK_WINDOW(win), 400, 300);
-    gtk_widget_add_events(GTK_WIDGET(win),
+    gtk_widget_add_events(GTK_WIDGET(event_box),
             GDK_BUTTON_PRESS_MASK |
             GDK_BUTTON_RELEASE_MASK |
             GDK_KEY_RELEASE_MASK |
             GDK_SCROLL_MASK);
 
-    g_signal_connect(G_OBJECT(win), "button-press-event",
+    g_signal_connect(G_OBJECT(event_box), "button-press-event",
             G_CALLBACK(mt_window_button_event), NULL);
-    g_signal_connect(G_OBJECT(win), "button-release-event",
+    g_signal_connect(G_OBJECT(event_box), "button-release-event",
             G_CALLBACK(mt_window_button_event), NULL);
     g_signal_connect(G_OBJECT(win), "key-release-event",
             G_CALLBACK(mt_window_key_release_event), NULL);
